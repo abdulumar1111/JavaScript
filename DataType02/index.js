b74bdfbb57c25fb126444513e11cc2f7a27161e0
abdulumar1111 <abdulumar1111@gmail.com>
2023-09-09 13:02:46 +0530

    Data Types

---
+++ DataType02/index.js
@@ -0,0 +1,19 @@
+let name = "Zeeshan";
+let age = 25;
+let isLoggedIn = true;
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+// Primitive Data Types
+// number => 2 power 53
+// string = ""
+// bigInt
+// boolean = true
+// null => standalone value
+// undefined
+// symbol => unique
+
+// Non Primitive Data Types
+// object
+// array
