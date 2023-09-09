13dd16cf8f5a27a21f010e1d97d23608865a9b16
abdulumar1111 <abdulumar1111@gmail.com>
2023-09-09 22:58:15 +0530

    Data Type Conversion

---
+++ DataConversion03/index.js
@@ -0,0 +1,62 @@
+// Number
+
+
+// let score = "24";
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+
+
+// let scoreValue = "33abc";
+// let scoreValueNumber = Number(scoreValue)
+// console.log(scoreValueNumber);
+
+
+
+// let rollNumber = null;
+// let scoreRollNumber = Number(rollNumber)
+// console.log(scoreRollNumber);
+
+
+
+// let accountId = undefined; 
+// let accountIdNumber = Number(accountId);
+// console.log(accountIdNumber);
+
+
+
+// let accountActive = true;
+// let accountActiveStatus = Number(accountActive);
+// console.log(accountActiveStatus);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+// Boolean
+// let isLoggedIn = 1;
+// let isLoggedInBoolean = Boolean(isLoggedIn);
+// console.log(isLoggedInBoolean)
+
+// let isLoggedIn = "";
+// let isLoggedInBoolean = Boolean(isLoggedIn);
+// console.log(isLoggedInBoolean)
+
+
+// let isLoggedIn = "Zeeshan";
+// let isLoggedInBoolean = Boolean(isLoggedIn);
+// console.log(isLoggedInBoolean)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "hi" => true
+
+// String
+
+let someNumber = 33;
+let someNumberString = String(someNumber);
+console.log(typeof someNumberString);
