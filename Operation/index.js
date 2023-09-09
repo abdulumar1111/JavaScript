5baaca9f2be7f9e0f626456885443e11ffa1520c
abdulumar1111 <abdulumar1111@gmail.com>
2023-09-09 23:29:44 +0530

    Operation

---
+++ Operation/index.js
@@ -0,0 +1,19 @@
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue)
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
