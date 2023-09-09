4efc69467644b6ce9fc026a6c18cc061d3fa913d
abdulumar1111 <abdulumar1111@gmail.com>
2023-09-09 11:42:37 +0530

    learn variable

---
+++ Variable01/index.js
@@ -0,0 +1,13 @@
+const accountId = 144405;
+let accountEmail = "abc123@gmail.com";
+var accountPassword = "12345";
+accountCity = "Bhadohi";
+let accountState;
+
+accountEmail = "db2233@gmail.com";
+accountPassword = "221234";
+accountCity = "Noida";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
