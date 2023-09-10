841266ad0f3d043fa9a17ad2ceee399a293d82d3
abdulumar1111 <abdulumar1111@gmail.com>
2023-09-10 14:29:24 +0530

    string method

---
+++ String06/index.js
@@ -0,0 +1,52 @@
+// let firstName = "Zeeshan";
+// let lastName = "Ansari";
+
+// console.log(`${firstName} ${lastName}`);
+
+// const gameName = new String("Vollyball");
+// console.log(gameName[0]);
+
+// length
+// console.log(gameName.length);
+
+// toUpperCase
+// console.log(gameName.toUpperCase());
+
+// charAt
+// console.log(gameName.charAt(0))
+
+// indexOf
+// console.log(gameName.indexOf('V'));
+
+// subString
+const fullName = new String("Zeeshan-Ansari");
+// const stringName = fullName.substring(0,4);
+// console.log(stringName);
+
+
+// Slice
+// const stringName = fullName.slice(-2,4);
+// console.log(stringName);
+
+
+// trim
+// const playerName = "      Zeeshan        ";
+// const playerNameTrim = playerName.trim();
+// console.log(playerName)
+// console.log(playerNameTrim);
+
+// replace
+// const url = "https://localhost:3000/ecommerce%20payment";
+// const urlReplace = url.replace('%20','/');
+// console.log(urlReplace);
+
+
+// includes
+// const url = "https://localhost:3000/ecommerce%20payment";
+// const urlIncludes = url.includes('localhost');
+// console.log(urlIncludes);
+
+// split
+const scoreBoard = new String('Zeeshan-Ansari-Bhadohi');
+const scoreBoardSplit = scoreBoard.split('-');
+console.log(scoreBoardSplit)
