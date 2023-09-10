1fb1abe1986257d73ae853202f88a3df57f9c159
abdulumar1111 <abdulumar1111@gmail.com>
2023-09-10 15:15:30 +0530

    Number

---
+++ Number07/index.js
@@ -0,0 +1,26 @@
+// const balance = new Number(1000);
+// console.log(balance)
+
+// toString
+// const balance = new Number(1000);
+// console.log(balance.toString())
+
+// toFixed
+// const balance = new Number(1000);
+// console.log(balance.toFixed(2))
+
+// toPrecision
+// const otherNumber = 123.875;
+// console.log(otherNumber.toPrecision(3));
+
+// toLocaleString
+// const hundred = 1000000;
+// console.log(hundred.toLocaleString('en-IN'))
+
+// MAX_VALUE
+// let x = Number.MAX_VALUE;
+// console.log(x);
+
+MIN_VALUE
+let x = Number.MIN_VALUE;
+console.log(x)
