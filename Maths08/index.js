2891b09eba86ce03e69cc4a366fdd86afe9cf672
abdulumar1111 <abdulumar1111@gmail.com>
2023-09-10 16:20:47 +0530

    Maths

---
+++ Maths08/index.js
@@ -0,0 +1,28 @@
+// abs
+// console.log(Math.abs(-6))
+
+// round
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.5))
+
+
+// ceil
+// console.log(Math.ceil(4.2));
+
+// floor
+// console.log(Math.floor(4.9))
+
+// min
+// console.log(Math.min(1,2,3,4,5))
+
+// max
+// console.log(Math.max(4,7,9,10,3,2))
+
+// random
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min) +1)+min)
