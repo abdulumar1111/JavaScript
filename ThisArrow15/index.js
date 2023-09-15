b0a31d8c6ce1ba48baf39cb47a6fd406bd6453c1
abdulumar1111 <abdulumar1111@gmail.com>
2023-09-15 23:46:03 +0530

    this and arrow function

---
+++ ThisArrow15/index.js
@@ -0,0 +1,81 @@
+// const user = {
+//     username : "Zeeshan",
+//     price: 199,
+//     welcomeMessage:function() {
+//         console.log(`${this.username} Welcome to this Websites`)
+//     }
+// }
+
+// user.welcomeMessage()
+
+
+// function One() {
+//     console.log(this);
+// }
+
+// One()
+
+
+
+// const One  = function() {
+//     let username = "Zeeshan";
+//     console.log(this.username);
+// }
+
+// One()
+
+
+// const One = () => {
+//     let username = "Zeeshan";
+//     console.log(this);
+// }
+
+// One()
+
+
+
+// const addTwoNumber = (num1,num2) => {
+//     return num1 + num2
+// }
+
+// console.log(addTwoNumber(10,20));
+
+
+// const addTwoNumber = (num1,num2) =>  num1 + num2
+
+// console.log(addTwoNumber(10,20));
+
+
+// const addTwoNumber = (num1,num2) =>  (num1 + num2)
+
+// console.log(addTwoNumber(10,20));
+
+
+// const userAuthenticate = () => ({
+//     username:'Zeeshan',
+//     password:'123'
+// })
+
+// console.log(userAuthenticate())
+
+
+
+// IIFE
+// (function One()  {
+//     console.log('DB Connect');
+// })();
+
+
+
+
+// (() =>   {
+//     console.log('DB Connect');
+// })()
+
+
+
+((name) =>   {
+    console.log(`Hello Mr ${name}`);
+})("Zeeshan Ansari")
+
+
