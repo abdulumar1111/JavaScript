ea994542a71a0d420f61a45cc720fdc95aa2238b
abdulumar1111 <abdulumar1111@gmail.com>
2023-09-23 22:55:22 +0530

    add Element

---
+++ Dom/7/index.js
@@ -0,0 +1,23 @@
+const parent = document.querySelector('.parent');
+// console.log(parent);
+// console.log(parent.children);
+// console.log(parent.children[0].innerHTML);
+
+// for(let i = 0; i < parent.children.length; i++) {
+//     console.log(parent.children[i].innerHTML);
+// }
+
+
+// Parent to Child
+
+parent.children[0].style.color = "red";
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+
+// const dayOne = document.querySelector('.day');
+// console.log(dayOne);
+// console.log(dayOne.parentElement);
+// console.log(dayOne.nextElementSibling);
+
+console.log(parent.childNodes);
